dc59e699b53ba97bd59011b74fa25d1d66f83217
kinto-b <kinto.behr@gmail.com>
2023-10-06 20:10:06 +1100

    Implement co-training

---
+++ pseudolabel.R
@@ -0,0 +1,69 @@
+
+source("rlib/pseudolabel_algorithms.R")
+
+# Load -------------------------------------------------------------------------
+
+set.seed(2023-10-06)
+stars <- read.csv("data/stars/prepared-train-car-0.001.csv")
+
+stars <- split(stars, is.na(stars$class))
+names(stars) <- c("labeled", "unlabeled")
+
+stars$validate <- read.csv("data/stars/prepared-validate.csv")
+
+
+# Self-train -------------------------------------------------------------------
+# Try a self-training with a few different classifiers
+
+cat("\n\n# Self-training with multinomial classifier ------- \n")
+selftrain(
+  class~., 
+  model = \(...) nnet::multinom(..., trace=FALSE),
+  predict_probs = \(m, df) predict(m, type = "probs", newdata = df),
+  stars$labeled, stars$unlabeled, stars$validate
+)
+
+cat("\n\n# Self-training with LDA classifier ------- \n")
+selftrain(
+  class~., 
+  model = \(...) MASS::lda(...),
+  predict_probs = \(m, df) predict(m, newdata = df)$posterior,
+  stars$labeled, stars$unlabeled, stars$validate
+)
+
+cat("\n\n# Self-training with QDA classifier ------- \n")
+selftrain(
+  class~., 
+  model = \(...) MASS::qda(...),
+  predict_probs = \(m, df) predict(m, newdata = df)$posterior,
+  stars$labeled, stars$unlabeled, stars$validate
+)
+
+cat("\n\n# Self-training with SVM classifier ------- \n")
+selftrain(
+  class~., 
+  model = \(...) kernlab::ksvm(..., prob.model=TRUE),
+  predict_probs = \(m, df) kernlab::predict(m, df, type="probabilities"),
+  stars$labeled, stars$unlabeled, stars$validate
+)
+
+
+
+# Co-train ---------------------------------------------------------------------
+# Try co-training with a few different pairs
+
+cat("\n\n# Co-training with multinomial/QDA classifiers ------- \n")
+cotrain(
+  class~., 
+  model1 = \(...) nnet::multinom(..., trace=FALSE),
+  predict_probs1 = \(m, df) predict(m, type = "probs", newdata = df),
+  
+  model2 = \(...) MASS::qda(...),
+  predict_probs2 = \(m, df) predict(m, newdata = df)$posterior,
+  
+  stars$labeled, stars$unlabeled, stars$validate,
+  epochs = 5
+)
+
+
+
---
+++ rlib/pseudolabel_algorithms.R
@@ -0,0 +1,186 @@
+#' Conduct self-training
+#' 
+#' Fit a model on the labeled data. Use the fitted model to predict classes
+#' for the unlabelled data. Add the predictions to the labeled data if they
+#' have high confidence. Repeat.
+#'
+#' @param formula A model formula
+#' @param model A function taking formula and data as its first two args and 
+#' returning a fitted model.
+#' @param predict_probs A function taking a fitted model and a new data.frame
+#'   and returning a matrix of fitted class probabilities
+#' @param labeled A data.frame to use as the labeled data
+#' @param unlabeled A data.frame to use as the unlabeled data
+#' @param test A data.frame to compute accuracy on
+#' @param alpha The psuedo-labelling probability cutoff. Unlabeled data will be
+#'   assigned to whatever class has probability greater than this value.
+#' @param epochs The maximum number of epochs to pseudo-label for.
+#'
+#' @return Invisibly, a list containing the final fitted model, the
+#'   pseudolabelled data and the remainders
+selftrain <- function(formula, model, predict_probs, labeled, unlabeled, test, alpha = 0.9, epochs = 10) {
+  lhs <- as.character(formula)[2]
+  data <- .prepare_data(formula, labeled, unlabeled, test)
+  cat(sprintf(
+    "Training on %d labeled and %d unlabeled...\n",
+    nrow(labeled),
+    nrow(unlabeled)
+  ))
+  
+  epoch <- 1
+  while (nrow(data$unlabeled)>1 && epoch <= epochs) {
+    m <- model(formula, data=data$labeled)
+    data <- .pseudolabel(formula, m, data, predict_probs, alpha)
+    
+    if (epoch == 1) {
+      acc0 <- .accuracy_selftrain(formula, m, predict_probs, data$test)
+      cat(sprintf("Starting accuracy %0.1f.\n", 100*acc0))
+    }
+    cat(sprintf("After epoch %d, %d unlabeled points remain.\n", epoch, nrow(data$unlabeled)))
+    epoch <- epoch + 1
+  }
+  
+  acc <- .accuracy_selftrain(formula, m, predict_probs, data$test)
+  cat(sprintf("Final accuracy %0.1f (%+0.1f).\n", 100*acc, 100*(acc-acc0)))
+  
+  invisible(list(
+    model = m,
+    pseudolabeled = labeled,
+    remainder = unlabeled
+  ))
+}
+
+
+#' Conduct co-training
+#' 
+#' Create two copies of the data. Fit one model on each copy of the labeled 
+#' data. Use model 1 to predict classes for unlabeled data 2. Add the predictions
+#' to labeled data 2 if they have high confidence. Do the same for model 2 and
+#' data 1. Repeat.
+#' 
+#' @param model1 A function taking formula and data as its first two args and 
+#' returning a fitted model.
+#' @param predict_probs1 A function taking a fitted model and a new data.frame
+#'   and returning a matrix of fitted class probabilities
+#' @param model2 A function taking formula and data as its first two args and 
+#' returning a fitted model.
+#' @param predict_probs2 A function taking a fitted model and a new data.frame
+#'   and returning a matrix of fitted class probabilities
+#' @inheritParams selftrain
+#'
+#' @return Invisibly, a list containing the final fitted models, the
+#'   pseudolabelled data and the remainders
+cotrain <- function(
+    formula, 
+    model1, predict_probs1, 
+    model2, predict_probs2,
+    labeled, unlabeled, test, 
+    alpha = 0.9, epochs = 10
+) {
+  lhs <- as.character(formula)[2]
+
+  data1 <- .prepare_data(formula, labeled, unlabeled, test)
+  data2 <- .prepare_data(formula, labeled, unlabeled, test)
+  cat(sprintf(
+    "Training on %d labeled and %d unlabeled...\n",
+    nrow(labeled),
+    nrow(unlabeled)
+  ))
+  
+  epoch <- 1
+  while (nrow(data1$unlabeled)>1 && nrow(data2$unlabeled)>1 && epoch <= epochs) {
+    m1 <- model1(formula, data=data1$labeled)
+    m2 <- model2(formula, data=data2$labeled)
+    
+    data1 <- .pseudolabel(formula, m2, data1, predict_probs2, alpha)
+    data2 <- .pseudolabel(formula, m1, data2, predict_probs1, alpha)
+    
+    if (epoch == 1) {
+      acc0 <- .accuracy_cotrain(formula, m1, predict_probs1, m2, predict_probs2, data1$test)
+      cat(sprintf("Starting accuracy %0.1f.\n", 100*acc0))
+    }
+    cat(sprintf(
+      "After epoch %d, (%d, %d) unlabeled points remain.\n",
+      epoch,
+      nrow(data1$unlabeled),
+      nrow(data2$unlabeled)
+    ))
+    epoch <- epoch + 1
+  }
+  
+  acc <- .accuracy_cotrain(formula, m1, predict_probs1, m2, predict_probs2, data1$test)
+  cat(sprintf("Final accuracy %0.1f (%+0.1f).\n", 100*acc, 100*(acc-acc0)))
+  
+  invisible(list(
+    model1 = m1,
+    model2 = m2,
+    pseudolabeled1 = data1$labeled,
+    pseudolabeled2 = data2$labeled,
+    remainder1 = data1$unlabeled,
+    remainder2 = data2$unlabeled
+  ))
+}
+
+
+
+# Internal ----------------------------------------------------------------
+
+.accuracy_selftrain <- function(formula, model, predict_probs, df) {
+  lhs <- as.character(formula)[2]
+  lvls <- levels(df[[lhs]])
+  
+  pred <- predict_probs(model, df)
+  pred <- lvls[apply(pred, 1, which.max)] # Class that maximises avg pred prob
+  pred <- factor(pred)
+  
+  mean(pred == df[[lhs]])
+}
+
+.accuracy_cotrain <- function(
+    formula, 
+    model1, predict_probs1, 
+    model2, predict_probs2,
+    df
+) {
+  lhs <- as.character(formula)[2]
+  lvls <- levels(df[[lhs]])
+  
+  pred1 <- predict_probs1(model1, df)
+  pred2 <- predict_probs2(model2, df)
+  pred <- (pred1 + pred2) / 2
+  pred <- lvls[apply(pred, 1, which.max)] # Class that maximises avg pred prob
+  pred <- factor(pred)
+  
+  mean(pred == df[[lhs]])
+}
+
+.prepare_data <- function(formula, labeled, unlabeled, test) {
+  lhs <- as.character(formula)[2]
+  
+  test[[lhs]] <- factor(test[[lhs]])
+  labeled[[lhs]] <- factor(labeled[[lhs]])
+  unlabeled[[lhs]] <- NA
+  
+  list(
+    labeled = labeled,
+    unlabeled = unlabeled,
+    test = test
+  )
+}
+
+.pseudolabel <- function(formula, fit, data, predict_probs, alpha) {
+  lhs <- as.character(formula)[2]
+  lvls <- levels(data$labeled[[lhs]])
+  
+  pred <- predict_probs(fit, data$unlabeled) > alpha
+  for (i in seq_along(lvls)) { 
+    data$unlabeled[[lhs]][pred[,i]] <- lvls[i]
+  }
+  
+  still_unlabeled <- is.na(data$unlabeled[[lhs]])
+  data$labeled <- rbind(data$labeled, data$unlabeled[!still_unlabeled, ])
+  data$unlabeled <- data$unlabeled[still_unlabeled, ]
+  
+  data
+}
+
