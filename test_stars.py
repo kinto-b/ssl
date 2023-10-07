a51fa0eb06e65180877196fcb15847a3541f4974
kinto-b <kinto.behr@gmail.com>
2023-10-08 09:44:35 +1100

    Add test script

---
+++ test_stars.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+def main():
+    # Initialise results file
+    results_fp = "data/results.csv"
+    with open(results_fp, "w", encoding="utf8") as f:
+        f.write("model,loss,accuracy\n")
+
+    # Load test data
+    test = pd.read_csv("data/stars/prepared-test.csv")
+    test["class"] = test["class"].replace({pd.NA: -1, "GALAXY": 0, "STAR": 1, "QSO": 2})
+    x = np.array(test.drop(columns=["class"]))
+    y = np.array(test["class"]).astype(np.int32)
+
+    # Evaluate models
+    model_names = os.listdir("data/models")
+    for model_name in model_names:
+        print(f"Evaluating {model_name}...")
+        model = tf.keras.models.load_model(f"data/models/{model_name}")
+
+        # We used a custom training loop, so we have to compile the model now
+        model.compile(
+            optimizer="adam",
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+        )
+
+        loss, acc = model.evaluate(x, y)
+
+        with open(results_fp, "a", encoding="utf8") as f:
+            f.write(f"{model_name},{loss},{acc}\n")
+
+
+if __name__ == "__main__":
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # FATAL
+    logging.getLogger("tensorflow").setLevel(logging.FATAL)
+    main()
--- train_stars.py
+++ train_stars.py
@@ -33,19 +33,19 @@ def main(epochs, prop_labeled=0.1):
     model_sl = get_model_toy_nn(in_shape, out_units)
     fit_sl = BaseModel(model_sl, optimizer, epochs)
     fit_sl.train(train_ds, test_ds, gamma=10)
-    model_sl.save(f"data/stars-{prop_labeled}-toy_nn-sl.keras")
+    model_sl.save(f"data/models/stars-{prop_labeled}-toy_nn-sl.keras")
 
     logger.info("Fitting pi model...")
     model_pi = get_model_toy_nn(in_shape, out_units)
     fit_pi = PiModel(model_pi, optimizer, epochs)
-    fit_pi.train(train_ds, test_ds, gamma=10, tau=epochs // 3)
-    model_pi.save(f"data/stars-{prop_labeled}-toy_nn-pi.keras")
+    fit_pi.train(train_ds, test_ds, gamma=1 / 3, tau=epochs // 3)
+    model_pi.save(f"data/models/stars-{prop_labeled}-toy_nn-pi.keras")
 
     logger.info("Fitting temporal ensemble...")
     model_te = get_model_toy_nn(in_shape, out_units)
     fit_te = TemporalEnsembleModel(model_te, optimizer, epochs)
-    fit_te.train(train_ds, test_ds, alpha=0.5, gamma=10, tau=epochs // 3)
-    model_pi.save(f"data/stars-{prop_labeled}-toy_nn-te.keras")
+    fit_te.train(train_ds, test_ds, alpha=0.5, gamma=1 / 3, tau=epochs // 3)
+    model_te.save(f"data/models/stars-{prop_labeled}-toy_nn-te.keras")
 
 
 if __name__ == "__main__":
