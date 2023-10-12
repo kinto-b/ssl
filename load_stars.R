2d3045cb994f0d13d5adfb1c00aac19eaf9bfafc
kinto-b <kinto.behr@gmail.com>
2023-10-12 14:34:48 +1100

    Create five CV folds

--- load_stars.R
+++ load_stars.R
@@ -12,6 +12,15 @@ library(dplyr)
 set.seed(2023-10-04)
 
 
+# Functions --------------------------------------------------------------------
+
+split_proportion <- function(tbl, prop) {
+  if (length(prop)==1) prop <- c(prop, 1-prop)
+  sp <- sample(seq_along(prop), size = nrow(tbl), replace = TRUE, prob = prop)
+  
+  split(tbl, sp)
+}
+
 # Prepare ----------------------------------------------------------------------
 
 stars <- read.csv("data/stars/raw.csv")
@@ -30,100 +39,79 @@ stars <- stars |>
   mutate(across(u:redshift, ~. - mean(.))) |> 
   mutate(across(u:redshift, ~. / sd(.))) 
 
-# TODO: Remove this
-# While models are still in development, we'll work with a 1/10th of the data
-# to speed things along
-# stars <- sample_n(stars, 10000)
+# Remove labels ----------------------------------------------------------------
+# We remove labels first completely at random, and then using the unit entropy.
+# To do this we produce selection indicators to tell whether each record is 
+# labeled or not.
 
-# Splits -----------------------------------------------------------------------
+# Completely at random
+proportions <- c(0.001, 0.01, 0.1, 0.25)
+lbl_mat_car <- sapply(proportions, \(p) runif(nrow(stars)) < p)
+colnames(lbl_mat_car) <- paste0("car-", proportions)
 
-split_proportion <- function(tbl, prop) {
-  if (length(prop)==1) prop <- c(prop, 1-prop)
-  sp <- sample(seq_along(prop), size = nrow(tbl), replace = TRUE, prob = prop)
-  
-  split(tbl, sp)
-}
-
-# First do a train/validate/test split
-dat <- split_proportion(stars, c(0.8, 0.1, 0.1))
-names(dat) <- c("train", "test", "validate")
-
-# Now assign make some of the training data unlabeled completely at random (CAR)
-unlabeled_proportions <- c(0.001, 0.01, 0.1, 0.25)
-train_car <- lapply(
-  unlabeled_proportions,
-  function(p) {
-    tr <- split_proportion(dat$train, p)
-    names(tr) <- c("labeled", "unlabeled")
-    tr$unlabeled$class <- NA
-    bind_rows(!!!tr)
-  }
-)
-names(train_car) <- unlabeled_proportions
-
-# We will up-sample the labeled data so we get equal sized labeled/unlabeled.
-train_car <- purrr::map(train_car, function(df) {
-  u <- df |> filter(is.na(class))
-  
-  l <- df |>
-    filter(!is.na(class)) |> 
-    sample_n(nrow(.env$u), replace = TRUE) 
-  
-  rbind(u, l)
-})
-
-# Write it out for modelling
-write.csv(dat$test, "data/stars/prepared-test.csv", row.names = FALSE)
-write.csv(dat$validate, "data/stars/prepared-validate.csv", row.names = FALSE)
-purrr::iwalk(train_car, \(x, p) write.csv(x, sprintf("data/stars/prepared-train-car-%s.csv", p), row.names = FALSE))
-
-
-# Entropy ----------------------------------------------------------------------
-# Ziyang has suggested sampling labeled data using entropy.
-
-# First we model the class probabilities
-m <- nnet::multinom(class~., dat$train)
+# Entropy
+m <- nnet::multinom(class~., stars)
 p <- predict(m, type = "probs")
-
-# Next we compute the case-level entropy
-p <- p + 1e-15 # Deal with predicted prob of zero
+p <- p + 1e-15 # p=0 leads to NaNs
 e <- apply(p, 1, \(p) - sum(p * log(p)))
-boxplot(e)
-p[which.max(e), ]
-p[which.min(e), ]
 
-# Finally we sample with probability Pr(i in I) = logistic(e)
-entropy_sampling_prob <- list(
-  `0.25` = plogis(1.25*e-1.25),
-  `0.1`  = plogis(1.25*e-2.25),
+proportions <- list(
+  `0.001` = plogis(1.25*e-7),
   `0.01` = plogis(1.25*e-4.75),
-  `0.001` = plogis(1.25*e-7)
+  `0.1`  = plogis(1.25*e-2.25),
+  `0.25` = plogis(1.25*e-1.25)
 )
 
-train_ent <- lapply(
-  entropy_sampling_prob,
-  function(p) {
-    idx <- runif(length(e)) < p
-    df <- dat$train
-    df$class[!idx] <- NA
-    df
-  }
+lbl_mat_ent <- sapply(proportions, \(p) runif(nrow(stars)) < p)
+colnames(lbl_mat_ent) <- paste0("ent-", names(proportions))
+
+stars <- bind_cols(stars, lbl_mat_car, lbl_mat_ent)
+
+# write.csv(stars, "data/stars/prepared.csv", row.names = FALSE)
+
+# Folds ------------------------------------------------------------------------
+# Now we'll create five cuts of the data for cross-validation.
+
+folds <- stars |> split_proportion(rep(0.2, 5))
+
+purrr::iwalk(folds, \(x, i) write.csv(x, sprintf("data/stars/prepared-fold%s.csv", i), row.names = FALSE))
+
+
+# README ------------------------------------------------------------------
+
+sink("data/stars/README.md")
+cat(
+  "# Stars", 
+  "",
+  "The raw data has been prepared by excluding one extreme outlier, dropping useless columns, and mean-centering and scaling the predictors so that they have sd=1.0.",
+  "",
+  "Indicator columns have been added to flag whether each observation should be treated as labeled or unlabeled.",
+  "For example, `car-0.1` contains approx. 10% TRUE selected completely at random while `ent-0.01` contains approx. 1% TRUE selected based on unit-entropy.",
+  "",
+  "Exact proportions are shown below.",
+  "", "",
+  sep = "\n"
 )
 
-lapply(train_ent, \(df) df |> count(is.na(class)) |> mutate(p=100*n/sum(n))) |> 
+stars |> 
+  select(`car-0.001`:`ent-0.25`) |> 
+  sapply(\(x) 100*table(x)/length(x)) |> 
   print()
 
-# We will up-sample the labeled data so we get equal sized labeled/unlabeled.
-train_ent <- purrr::map(train_ent, function(df) {
-  u <- df |> filter(is.na(class))
-  
-  l <- df |>
-    filter(!is.na(class)) |> 
-    sample_n(nrow(.env$u), replace = TRUE) 
-  
-  rbind(u, l)
-})
+cat("\n\n")
+cat(
+  "To use the data, do something like", "",
+  "```",
+  "df <- read.csv('prepared-fold1.csv') |>",
+  "  mutate(class = ifelse(`car-0.1`, class, NA) # Unlabel",
+  "  select(u:class) # Drop selection indicators",
+  "```", "",
+  "We can do CV by training on 4 of the folds and testing on the remaining held-out fold.",
+  sep = "\n"
+)
+
+sink()
 
 
-purrr::iwalk(train_ent, \(x, p) write.csv(x, sprintf("data/stars/prepared-train-ent-%s.csv", p), row.names = FALSE))
+
 
